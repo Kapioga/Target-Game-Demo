3810247bdd9506fd1fe7e1400bbe4add54645419
Kapioga <ldaleyvo@gmail.com>
2022-09-06 17:19:01 -0400

    Practice day 3

--- main.lua
+++ main.lua
@@ -1,11 +1,24 @@
-function _load()
+function love.load() --Start
+    target = {}
+    target.x = 300
+    target.y = 300
+    target.radius = 50
+
+    score = 0
+    timer = 0
+
+    gameFont = love.graphics.newFont(40)
+end
+
+function love.update(dt) --Delta Time (60FPS)
 
 end
 
-function _update()
+function love.draw()
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", target.x, target.y, target.radius)
 
-end
-
-function _draw()
-
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(gameFont)
+    love.graphics.print(score, 10, 10)
 end
